9da34dd670eb6b99a63539c2835bddceb7e4f9ac
Marc Anthony Surio <suriomanthony@gmail.com>
2020-10-31 19:53:16 -0700

    within the initial creation of the files, I added a routes folder called 'app.js' and 'view.js'. I ran a npm init and install dependencies.

---
+++ routes/view.js
@@ -0,0 +1,15 @@
+// requires
+const router = require("express").Router();
+const path = require("path");
+
+// routes
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+//export
+module.exports = router;
